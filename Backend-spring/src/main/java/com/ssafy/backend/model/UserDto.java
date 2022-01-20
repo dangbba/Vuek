876e393fb9ac884da7e38d924e4de8fc0ab16d1a
kimseonghyun <gkska741@kaist.ac.kr>
2022-01-20 11:39:21 +0900

    BE:버전관리/김성현

--- Backend-spring/src/main/java/com/ssafy/backend/model/UserDto.java
+++ Backend-spring/src/main/java/com/ssafy/backend/model/UserDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class UserDto {
-	private String user_id;
-	private String user_name;
-	private String password;
+    private String user_id;
+    private String user_name;
+    private String password;
 }
