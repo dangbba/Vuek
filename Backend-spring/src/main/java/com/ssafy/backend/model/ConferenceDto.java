f4f0936877efae243fbc2f84134f6f9bc470f41b
ooodraft_ <ooodraft_@naver.com>
2022-01-24 17:56:21 +0900

    BE:conference매퍼수정/박설

--- Backend-spring/src/main/java/com/ssafy/backend/model/ConferenceDto.java
+++ Backend-spring/src/main/java/com/ssafy/backend/model/ConferenceDto.java
@@ -7,7 +7,7 @@ public class ConferenceDto {
 	private int id;
 	private int user_user_id;
 	private int book_detail_id;
-	private int conference_type_name;
+	private int conference_type;
 	private String call_start_time;
 	private String call_end_time;
 	private String thumbnail_url;
--- Backend-spring/src/main/java/com/ssafy/backend/model/ConferenceInfoDto.java
+++ Backend-spring/src/main/java/com/ssafy/backend/model/ConferenceInfoDto.java
@@ -7,7 +7,7 @@ public class ConferenceInfoDto {
 	private int id;
 	private int user_user_id;
 	private int book_detail_id;
-	private int conference_type_name;
+	private int conference_type;
 	private String call_start_time;
 	private String thumbnail_url;
 	private String title;
