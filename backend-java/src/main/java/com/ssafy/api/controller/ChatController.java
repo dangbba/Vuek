900d59f9d88f6bf1115e58398ffaeea5a1e95eb5
ooodraft_ <ooodraft_@naver.com>
2022-02-13 14:14:08 +0900

    BE:chatting/박설

--- backend-java/src/main/java/com/ssafy/api/controller/ChatController.java
+++ backend-java/src/main/java/com/ssafy/api/controller/ChatController.java
@@ -1,23 +1,71 @@
 package com.ssafy.api.controller;
 
-import com.ssafy.db.vo.SocketVO;
+import com.ssafy.api.service.ChatService;
+import com.ssafy.api.service.ChatServiceImpl;
+import com.ssafy.db.dto.ChatMessageDto;
+import com.ssafy.db.dto.ChatRoomDto;
+import com.ssafy.db.repository.ChatRoomRepository;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+//@Controller
+//public class ChatController {
+//
+//	@MessageMapping("/receive")
+//	@SendTo("/send")
+//
+//	public SocketVO SocketHandler(SocketVO socketVO) {
+//		String userName = socketVO.getUserName();
+//		String content = socketVO.getContent();
+//
+//		SocketVO result = new SocketVO(userName, content);
+//		return result;
+//	}
+//
+//}
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@RestController
+@RequestMapping("/api/v1/chat")
 public class ChatController {
-	
-	@MessageMapping("/receive")
-	@SendTo("/send")
-	
-	public SocketVO SocketHandler(SocketVO socketVO) {
-		String userName = socketVO.getUserName();
-		String content = socketVO.getContent();
-		
-		SocketVO result = new SocketVO(userName, content);
-		return result;
+
+	@Autowired
+	private ChatService chatService;
+
+	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public ChatMessageDto register(@Payload ChatMessageDto chatMessageDto, SimpMessageHeaderAccessor headerAccessor) {
+		return chatService.register(chatMessageDto, headerAccessor);
 	}
+
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public ChatMessageDto sendMessage(@Payload ChatMessageDto chatMessageDto) {
+		return chatMessageDto;
+	}
+
+ 	@MessageMapping("/chat.leave")
+	public void leaveRoom(@Payload ChatMessageDto chatMessageDto) {
+		chatService.leaveRoom(chatMessageDto);
+	}
+
+	@GetMapping("/room/{idconference}")
+	public ChatRoomDto getChatRoomById(@PathVariable long idconference){
+		return chatService.getChatRoomById(idconference);
+	}
+
+	@PostMapping
+	@ResponseBody
+	public long createChatRoom(@RequestParam long idconference){
+		return chatService.createChatRoom(idconference);
+	}
+
 
 }
--- backend-java/src/main/java/com/ssafy/db/dto/ChatRoomDto.java
+++ backend-java/src/main/java/com/ssafy/db/dto/ChatRoomDto.java
@@ -1,24 +1,59 @@
 package com.ssafy.db.dto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
+import org.checkerframework.checker.units.qual.C;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import javax.persistence.*;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
-@ToString
 @Data
 public class ChatRoomDto {
-    private int Conferenceid;
+
+    private long conferenceId;
+    @ElementCollection(fetch = FetchType.LAZY)
     private Set<WebSocketSession> sessions = new HashSet<>();
 
-    public static ChatRoomDto create(String name){
+    public static ChatRoomDto create(long idconference){
         ChatRoomDto room = new ChatRoomDto();
+        room.conferenceId = idconference;
         return room;
+    }
+
+    public void handleMessage(WebSocketSession session, ChatMessageDto chatMessage,
+                              ObjectMapper objectMapper) throws IOException {
+        if(chatMessage.getType() == ChatMessageDto.MessageType.ENTER){
+            sessions.add(session);
+            chatMessage.setContent(chatMessage.getSender() + "님이 입장하셨습니다.");
+        }
+        else if(chatMessage.getType() == ChatMessageDto.MessageType.LEAVE){
+            sessions.remove(session);
+            chatMessage.setContent(chatMessage.getSender() + "님임 퇴장하셨습니다.");
+        }
+        else{
+            chatMessage.setContent(chatMessage.getSender() + " : " + chatMessage.getContent());
+        }
+        send(chatMessage,objectMapper);
+    }
+
+    private void send(ChatMessageDto chatMessage, ObjectMapper objectMapper) throws IOException {
+        TextMessage textMessage = new TextMessage(objectMapper.
+                writeValueAsString(chatMessage.getContent()));
+        for(WebSocketSession sess : sessions){
+            sess.sendMessage(textMessage);
+        }
     }
 }
