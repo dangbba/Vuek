7d8ac88223adf9dea4956074f6355ed6bb3bf94f
ooodraft_ <ooodraft_@naver.com>
2022-02-15 20:17:39 +0900

    BE:수정

--- backend-java/src/main/java/com/ssafy/config/WebSocketConfig.java
+++ backend-java/src/main/java/com/ssafy/config/WebSocketConfig.java
@@ -1,10 +1,9 @@
 package com.ssafy.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -21,4 +20,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
+
 }
