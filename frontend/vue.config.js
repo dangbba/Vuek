f234a252adfd3f62cf5cb262db29ad179a79b149
문찬송 <moon.sw.good@gmail.com>
2022-02-15 21:46:42 +0900

    FE:dist file 교체(빌드시 title한글깨짐 있음)/문찬송

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,5 +4,17 @@ module.exports = {
     https:true,
     port: 8083,
     open: true,
-  }
+  },
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = '모두의 책갈피'
+      return args
+    })
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+  },
+  outputDir: '../backend-java/src/main/resources/dist'
 };
