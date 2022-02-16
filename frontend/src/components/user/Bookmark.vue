2f1731f1a8fae62e6bed44db7dfbcf4e5753e0c4
문찬송 <moon.sw.good@gmail.com>
2022-02-17 00:31:26 +0900

    FE:책갈피 기능 관련 수정/문찬송

--- frontend/src/components/user/Bookmark.vue
+++ frontend/src/components/user/Bookmark.vue
@@ -4,8 +4,26 @@
     <i class="far fa-bookmark" style="color: #0dcaf0;"></i>
       나의 책갈피
   </p> <!-- 폰트어썸 아이콘 -->
-    <b-button size="sm" v-b-modal.modal-prevent-closing variant="warning" @click="getUserBook(userInfo.userId)">확인하기</b-button>
-    <b-modal scrollable id="modal-prevent-closing" ref="modal" title="나의 책갈피">
+    <b-button size="sm" v-b-modal.modal-prevent-closing variant="warning" class="mb-5" @click="getUserBook(userInfo.userId)">확인하기</b-button>
+    <b-modal scrollable size="lg" ok-only ok-title="확인" ok-variant="outline-primary" id="modal-prevent-closing" ref="modal" title="나의 책갈피">
+      <!-- {{ userBookmark }} -->
+      <div v-if="userBookmark.length === 0">
+        <h5 class="text-center">현재 책갈피된 도서가 없습니다. 도서를 검색하여 책갈피를 등록해주세요.</h5>
+      </div>
+      <div v-else>
+        <b-card v-for="bookmark, index in userBookmark" :key="index" :img-src="transUrl(bookmark.titleUrl)" img-width="150px" img-alt="book image" img-left class="mb-3">
+          <b-card-text>
+            도서명: {{ transStr(bookmark.title) }}
+            <hr>
+            저자: {{ transStr(bookmark.author) }} / 출판사: {{ transStr(bookmark.publisher) }}
+            <div v-if="bookmark.overview">
+              <hr>
+              줄거리: {{ transStr(bookmark.overview) }}
+            </div>
+          </b-card-text>
+        </b-card>
+      </div>
+    
     </b-modal>
   </div>
 </template>
@@ -21,6 +39,20 @@ export default {
   },
   methods:{
     ...mapActions("bookStore", ['getUserBook']),
+    transUrl(url) {
+      let transedUrl = ""
+      if(url) {
+        transedUrl = url.replace('=m1&', '=m200&') 
+      } else {
+        transedUrl = require('@/assets/thumbnail/thumbnail_default_img.jpg') 
+      }
+      return transedUrl
+    },
+    transStr(str) {
+      let transedStr = str.replaceAll('<b>', '')
+      transedStr = transedStr.replaceAll('</b>', '')
+      return transedStr
+    },
   },
   computed:{
     ...mapState("userStore", ["userInfo"]),
--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,4 +1,4 @@
-//const BASE_URL = "https://localhost:8080/api/v1";
-//const BASE_URL = "https://localhost:8443/api/v1";
+// const BASE_URL = "https://localhost:8080/api/v1";
+// const BASE_URL = "https://localhost:8443/api/v1";
 const BASE_URL = "https://i6c205.u.ssafy.io/api/v1";
 export { BASE_URL };
